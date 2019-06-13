0685cc279bba194adb2d0e1bf615d85d0c655b54
Dmitriy Serebryandkiy <serebryanskiy.dmitriy@gmail.com>
2019-06-13 09:02:17 +0700

    restart

--- restart.py
+++ restart.py
@@ -1,10 +1,12 @@
 import json
-import subprocess
+import subprocess                                                                                                                              
 
-with open('/run/hive/gpu-stats.json','r') as json_file:
-    data = json.load(json_file)
-    load = set([gpu for gpu in data['load']])
 
-if len(load) == 1 and load[0] == 0:
-    subprocess.run(["miner", "stop"])
+with open('/run/hive/gpu-stats.json','r') as json_file:                                                                                        
+    data = json.load(json_file)                                                                                                                
+    load = set([float(gpu) for gpu in data['load']])                                                                                           
+
+if len(load) == 1 and list(load)[0] == 0:                                                                                                      
+    subprocess.run(["miner", "stop"])                                                                                                          
     subprocess.run(["miner", "start"])
+
